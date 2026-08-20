42dce932141354f213bc55db3e92e34bf81b8c2b
Gustavo <tomazinigustavo2@gmail.com>
2026-08-20 15:19:53 -0400

    Caso o nome esteja em branco, o código não avança.

--- GatewayWebAPI/Program.cs
+++ GatewayWebAPI/Program.cs
@@ -14,33 +14,38 @@ Dictionary<string, double> BancoDeDadosSaldos = new Dictionary<string, double>
 app.MapPost("/pagar", (RequisicaoPagamento dados) =>
 {
 
+    if (string.IsNullOrWhiteSpace(dados.Cliente))
+    {
+        return Results.BadRequest(new { mensagem = "O nome do cliente não pode estar em branco!" });
+    }
+
     if (!BancoDeDadosSaldos.ContainsKey(dados.Cliente))
-    {
-        return Results.BadRequest(new { mensagem = "Erro: Cliente não encontrado no banco de dados." });
-    }
-
-    double saldoBanco = BancoDeDadosSaldos[dados.Cliente];
-
-    if (dados.Cartao && saldoBanco >= dados.Compra)
-    {
-        BancoDeDadosSaldos[dados.Cliente] = saldoBanco - dados.Compra;
-        double novosaldo = BancoDeDadosSaldos[dados.Cliente];
-
-        return Results.Ok(new
         {
-            status = "APROVADO",
-            cliente = dados.Cliente,
-            novoSaldo = novosaldo
-        });
-    }
-    else
-    {
-        return Results.BadRequest(new
+            return Results.BadRequest(new { mensagem = "Erro: Cliente não encontrado no banco de dados." });
+        }
+
+        double saldoBanco = BancoDeDadosSaldos[dados.Cliente];
+
+        if (dados.Cartao && saldoBanco >= dados.Compra)
         {
-            status = "RECUSADO",
-            motivo = "Saldo insuficiente ou cartão inválido."
-        });
-    }
+            BancoDeDadosSaldos[dados.Cliente] = saldoBanco - dados.Compra;
+            double novosaldo = BancoDeDadosSaldos[dados.Cliente];
+
+            return Results.Ok(new
+            {
+                status = "APROVADO",
+                cliente = dados.Cliente,
+                novoSaldo = novosaldo
+            });
+        }
+        else
+        {
+            return Results.BadRequest(new
+            {
+                status = "RECUSADO",
+                motivo = "Saldo insuficiente ou cartão inválido."
+            });
+        }
 });
 
 app.MapPost("/cadastrar", (Requisicaocadastro dados) =>
