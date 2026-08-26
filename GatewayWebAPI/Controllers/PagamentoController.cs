bf28148e46e09db924569b37f1f0dbe3ac3a3696
Gustavo <tomazinigustavo2@gmail.com>
2026-08-26 09:17:33 -0400

    Organização das rotas com implementação de PagamentoController.

---
+++ GatewayWebAPI/Controllers/PagamentoController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using GatewayWebAPI.Models;
+using GatewayWebAPI.Data;
+
+namespace GatewayWebAPI.Controllers
+{
+    public static class PagamentoController
+    {
+        public static IResult ProcessarPagamento(RequisicaoPagamento dados, AppDbContext banco)
+        {
+
+            Console.WriteLine($"[{DateTime.Now}] INFO: Requisição de pagamentos recebida para o cliente '{dados.Cliente}'");
+
+            if (string.IsNullOrWhiteSpace(dados.Cliente))
+            {
+                Console.WriteLine($"[{DateTime.Now}] ERRO: O nome do cliente está em branco '{dados.Cliente}'");
+                return Results.BadRequest(new { mensagem = "O nome do cliente não pode estar em branco!" });
+            }
+
+            try
+            {
+                var clienteNoBanco = banco.Clientes.FirstOrDefault(c => c.Nome == dados.Cliente);
+
+                if (clienteNoBanco == null)
+                {
+                    Console.WriteLine($"[{DateTime.Now}] ERRO: O cliente não está no banco de dados '{dados.Cliente}'");
+                    return Results.BadRequest(new { mensagem = "Erro: Cliente não encontrado no banco de dados." });
+                }
+                else if (dados.Compra > 0 && clienteNoBanco.Saldoinicial >= dados.Compra)
+                {
+                    Console.WriteLine($"[{DateTime.Now}] INFO: Compra bem sucedida para o cliente '{dados.Cliente}'");
+                    clienteNoBanco.Saldoinicial = clienteNoBanco.Saldoinicial - dados.Compra;
+                    banco.SaveChanges();
+                    return Results.Ok(new { mensagem = "Compra bem sucedida!" });
+                }
+                else
+                {
+                    Console.WriteLine($"[{DateTime.Now}] ERRO: Valor 0 ou inferior OU saldo insuficiente");
+                    return Results.BadRequest(new { mensagem = "O valor da compra não pode ser zero ou inferior ou saldo insuficiente" });
+                }
+            }
+            catch (Exception erro)
+            {
+                Console.WriteLine($"[{DateTime.Now}] CRÍTICO: Problema no SQL. Detalhes: '{erro.Message}'");
+                return Results.Problem("Desculpe, o sistema de pagamentos está instável. Tente novamente mais tarde.");
+            }
+        }
+
+        public static IResult ProcessarCheckout(RequisicaoPagamento dados, AppDbContext banco)
+        {
+
+            Console.WriteLine($"[{DateTime.Now}] INFO: Iniciando Checkout de {dados.Cliente}");
+
+            if (string.IsNullOrWhiteSpace(dados.Cliente))
+            {
+
+                Console.WriteLine($"[{DateTime.Now}] ERRO: Nome vazio '{dados.Cliente}'");
+
+                return Results.BadRequest(new { mensagem = "O nome do cliente não pode vir em branco" });
+            }
+
+            try
+            {
+                var clientenobanco = banco.Clientes.FirstOrDefault(c => c.Nome == dados.Cliente);
+
+                if (clientenobanco == null)
+                {
+                    var novoCliente = new Requisicaocadastro
+                    {
+                        Nome = dados.Cliente,
+                        Saldoinicial = 500
+                    };
+
+                    banco.Clientes.Add(novoCliente);
+                    banco.SaveChanges();
+
+                    clientenobanco = novoCliente;
+                }
+
+                Console.WriteLine($"[{DateTime.Now}] INFO: Iniciando processo de compra para {dados.Cliente}");
+
+                if (dados.Compra <= 0 || clientenobanco.Saldoinicial < dados.Compra)
+                {
+
+                    Console.WriteLine($"[{DateTime.Now}] ERRO: Valor ou saldo insuficiente {dados.Cliente}");
+
+                    return Results.BadRequest(new { mensagem = "O valor da compra não pode ser 0 ou menor ou não há saldo suficiente." });
+                }
+
+                clientenobanco.Saldoinicial = clientenobanco.Saldoinicial - dados.Compra;
+                banco.SaveChanges();
+
+                Console.WriteLine($"[{DateTime.Now}] INFO: Checkout concluído com sucesso (pago) para {dados.Cliente}");
+
+                return Results.Ok(new { status = "CONCLUÍDO!", mensagem = "Checkout completo.", novosaldo = clientenobanco.Saldoinicial });
+
+            }
+
+            catch (Exception erro)
+            {
+                Console.WriteLine($"[{DateTime.Now}] CRÍTICO: O SQL deu problema. Detalhes: '{erro.Message}'");
+                return Results.Problem("O sistema de pagamentos está com problemas nesse momento, pedimos desculpas. Tente novamente mais tarde.");
+            }
+
+
+        }
+    }
+
+}
--- GatewayWebAPI/Program.cs
+++ GatewayWebAPI/Program.cs
@@ -1,6 +1,7 @@
 using GatewayWebAPI.Models;
 using GatewayWebAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using GatewayWebAPI.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,45 +26,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapPost("/pagar", (RequisicaoPagamento dados, AppDbContext banco) =>
-{
-    Console.WriteLine($"[{DateTime.Now}] INFO: Requisição de pagamentos recebida para o cliente '{dados.Cliente}'");
-
-    if (string.IsNullOrWhiteSpace(dados.Cliente))
-    {
-        Console.WriteLine($"[{DateTime.Now}] ERRO: O nome do cliente está em branco '{dados.Cliente}'");
-        return Results.BadRequest(new { mensagem = "O nome do cliente não pode estar em branco!" });
-    }
-
-    try
-    {
-        var clienteNoBanco = banco.Clientes.FirstOrDefault(c => c.Nome == dados.Cliente);
-
-        if (clienteNoBanco == null)
-        {
-            Console.WriteLine($"[{DateTime.Now}] ERRO: O cliente não está no banco de dados '{dados.Cliente}'");
-            return Results.BadRequest(new { mensagem = "Erro: Cliente não encontrado no banco de dados." });
-        }
-        else if (dados.Compra > 0 && clienteNoBanco.Saldoinicial >= dados.Compra)
-        {
-            Console.WriteLine($"[{DateTime.Now}] INFO: Compra bem sucedida para o cliente '{dados.Cliente}'");
-            clienteNoBanco.Saldoinicial = clienteNoBanco.Saldoinicial - dados.Compra;
-            banco.SaveChanges();
-            return Results.Ok(new { mensagem = "Compra bem sucedida!" });
-        }
-        else
-        {
-            Console.WriteLine($"[{DateTime.Now}] ERRO: Valor 0 ou inferior OU saldo insuficiente");
-            return Results.BadRequest(new { mensagem = "O valor da compra não pode ser zero ou inferior ou saldo insuficiente" });
-        }
-    }
-    catch (Exception erro)
-    {
-        Console.WriteLine($"[{DateTime.Now}] CRÍTICO: Problema no SQL. Detalhes: '{erro.Message}'");
-        return Results.Problem("Desculpe, o sistema de pagamentos está instável. Tente novamente mais tarde.");
-    }
-
-});
+app.MapPost("/pagar", PagamentoController.ProcessarPagamento);
 
 app.MapPost("/cadastrar", (Requisicaocadastro dados, AppDbContext banco) =>
 {
@@ -83,62 +46,6 @@ app.MapPost("/cadastrar", (Requisicaocadastro dados, AppDbContext banco) =>
     }
 });
 
-app.MapPost("/checkout", (RequisicaoPagamento dados, AppDbContext banco) =>
-{
-
-    Console.WriteLine($"[{DateTime.Now}] INFO: Iniciando Checkout de {dados.Cliente}");
-
-    if (string.IsNullOrWhiteSpace(dados.Cliente))
-    {
-
-        Console.WriteLine($"[{DateTime.Now}] ERRO: Nome vazio '{dados.Cliente}'");
-
-        return Results.BadRequest(new { mensagem = "O nome do cliente não pode vir em branco" });
-    }
-
-    try
-    {
-        var clientenobanco = banco.Clientes.FirstOrDefault(c => c.Nome == dados.Cliente);
-
-        if (clientenobanco == null)
-        {
-            var novoCliente = new Requisicaocadastro
-            {
-                Nome = dados.Cliente,
-                Saldoinicial = 500
-            };
-
-            banco.Clientes.Add(novoCliente);
-            banco.SaveChanges();
-
-            clientenobanco = novoCliente;
-        }
-
-        Console.WriteLine($"[{DateTime.Now}] INFO: Iniciando processo de compra para {dados.Cliente}");
-
-        if (dados.Compra <= 0 || clientenobanco.Saldoinicial < dados.Compra)
-        {
-
-            Console.WriteLine($"[{DateTime.Now}] ERRO: Valor ou saldo insuficiente {dados.Cliente}");
-
-            return Results.BadRequest(new { mensagem = "O valor da compra não pode ser 0 ou menor ou não há saldo suficiente." });
-        }
-
-        clientenobanco.Saldoinicial = clientenobanco.Saldoinicial - dados.Compra;
-        banco.SaveChanges();
-
-        Console.WriteLine($"[{DateTime.Now}] INFO: Checkout concluído com sucesso (pago) para {dados.Cliente}");
-
-        return Results.Ok(new { status = "CONCLUÍDO!", mensagem = "Checkout completo.", novosaldo = clientenobanco.Saldoinicial });
-
-    }
-
-    catch (Exception erro)
-    {
-        Console.WriteLine($"[{DateTime.Now}] CRÍTICO: O SQL deu problema. Detalhes: '{erro.Message}'");
-        return Results.Problem("O sistema de pagamentos está com problemas nesse momento, pedimos desculpas. Tente novamente mais tarde.");
-    }
-
-});
+app.MapPost("/checkout", PagamentoController.ProcessarCheckout); 
 
 app.Run();
